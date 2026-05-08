5e320f7f3d408dd79ae28a40c510b4b9ed0fbc81
Eesge Masekowsky <masekowsky.ee@gmail.com>
2026-05-08 12:53:17 +0000

    adjusted phrases and repaired string builder

--- generator/gen.js
+++ generator/gen.js
@@ -10,27 +10,34 @@ let first = [
     'YOUR MOM',
     'THE BUS DRIVER IN MIDDLE SCHOOL',
     'MY BOSS',
-    'MICHAEL JACKSON'
+    'MICHAEL JACKSON',
+    'THE NEIGHBOURS',
+    'THE DEMOCRATIC REPUBLIC OF KONGO'
 ];
 let second = [
-    'CAN', 'SHOULD', 'MUST', 'NEEDS TO', 'WILL', 'IS GOING TO', 
-    'CAN\'T', 'SHOULDN\'T', 'OUGHT NOT TO', 'WON\'T', 'ISN\'T GOING TO'
+    'CAN', 'SHOULD', 'MUST', 'OUGHT TO', 'WILL', 
+    'CAN\'T', 'SHOULDN\'T', 'OUGHT NOT TO', 'WON\'T',
 ];
 let third = [
     'GO', 'WIN', 'LOSE', 'STAY', 'RUN', 'DIE', 'GO OUT AND PARTY', 'HAVE SOME ICECREAM', 'PRAY TONIGHT', 'HAVE A NIGHTMARE', 'FIND 50 BUCKS',
-    'MAKE A DAD JOKES', 'SKIP CLASS TOMORROW', 'GET MORE MONEY'
+    'MAKE A DAD JOKES', 'SKIP CLASS TOMORROW', 'GET MORE MONEY', 'FORGET TO HAVE BREAKFAST', 'HEAR THE ALARM TOMORROW MORNING'
 ];
 
 let messageArray = [];
 
+console.log('This is the oracle of Okemos.')
+
 for (i = 0; i < 10; i++){
     let newString = '';
-    newString = newString + Math.floor(Math.random()*first.length);
-    newString = newString + ' ' + Math.floor(Math.random()*second.length);
-    newString = newString + ' ' + Math.floor(Math.random()*third.length);
+    newString = newString + first[Math.floor(Math.random()*first.length)];
+    newString = newString + ' ' + second[Math.floor(Math.random()*second.length)];
+    newString = newString + ' ' + third[Math.floor(Math.random()*third.length)];
     messageArray.push(newString);
 }
 
 for (message of messageArray) {
+    console.log('Oh great oracle, can you tell us something wise?')
     console.log(message);
 }
+
+console.log('Thank you for your time...');
